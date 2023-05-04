aa01c163f2966d6544a7c8b072d7161c8fc275c8
nicanico <nicsousan@gmail.com>
2023-05-04 16:50:24 -0300

    04/05 - inicio das rotas

---
+++ js/routes.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const routes = {
+    '/' : '/pages/home.html',
+    '/personagens' : '../pages/personagens.html',
+    '/localizacao' : '/pages/localizacao.html',
+    '/episodio' : '/pages/episodio.html'
+}
+
+const route = async () => {
+
+}
+
+window.route = route
