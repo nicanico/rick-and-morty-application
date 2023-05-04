bddd31f7bb9543532a5ce628baf63d2c333eefc8
nicanico <nicsousan@gmail.com>
2023-05-03 21:12:05 -0300

    03/05 - layout ainda com quebras e inicio de rotas

--- js/components-card.js
+++ js/components-card.js
@@ -1,10 +1,20 @@
 'use strict'
 
-class card extends HTMLElement {
+class cardGeral extends HTMLElement {
     constructor() {
         super();
         
         this.shadow = this.attachShadow({mode: open})
+        this.namePersonagem = 'Nome do Personagem'
+        this.status = 'Status do personagem'
+        this.especie = 'Especie'
+        this.genero = 'Genero'
+        this.origem = 'Origem'
+        this.foto = null
+    }
+
+    static get observedAttributes(){
+        return ['']
     }
 }
 
