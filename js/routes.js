c3252415d191b98d381dc5b9eb1500d4d6938b36
nicanico <nicsousan@gmail.com>
2023-05-04 17:00:25 -0300

    04/05 - rotas

--- js/routes.js
+++ js/routes.js
@@ -8,7 +8,19 @@ const routes = {
 }
 
 const route = async () => {
+    window.event.oreventDefalut()
 
+    window.history.pushState({}, "", window.event.target.href)
+
+    const path = window.location.pathname
+
+    const response = await fetch(routes[path])
+    const html = await response.text()
+
+    console.log(html)
+    console.log(routes[path])
+
+    document.getElementById('root').innerHTML = html
 }
 
 window.route = route
